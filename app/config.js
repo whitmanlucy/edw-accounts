20cea7f5e0202c9a5840eb2bcf63110bcb19b5c4
jeff <jeff@eatdrinkweb.com>
2015-04-01 22:52:38 -0700

    updated npm, bower dependencies configured unit and e2e testing

--- app/config.js
+++ app/config.js
@@ -3,6 +3,9 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp.config', [])
 
+    //Navbar partial view
+    .constant('navbarPath', 'partials/navbar.html')
+
   // version of this seed app is compatible with angularFire 1.0.0
   // see tags for other versions: https://github.com/firebase/angularFire-seed/tags
   .constant('version', '1.0.0')
@@ -11,7 +14,8 @@ angular.module('myApp.config', [])
   .constant('loginRedirectPath', '/login')
 
   // your Firebase data URL goes here, no trailing slash
-  .constant('FBURL', 'https://angularfire-seed-dev.firebaseio.com')
+  //.constant('FBURL', 'https://angularfire-seed-dev.firebaseio.com')
+  .constant('FBURL', 'https://EDW-AF2.firebaseio.com')
 
   // double check that the app has been configured before running it and blowing up space and time
   .run(['FBURL', '$timeout', function(FBURL, $timeout) {
--- e2e-tests/protractor-conf.js
+++ e2e-tests/protractor-conf.js
@@ -2,14 +2,16 @@ exports.config = {
   allScriptsTimeout: 11000,
 
   specs: [
-    'e2e/*.js'
+    '../e2e-tests/*.js'
   ],
 
   capabilities: {
     'browserName': 'chrome'
   },
 
-  baseUrl: 'http://localhost:8000/app/',
+  //baseUrl: 'http://localhost:8000/app/',
+  baseUrl: 'http://local.eatdrinkweb.com/edw-accounts/app/',
+
 
   framework: 'jasmine',
 
